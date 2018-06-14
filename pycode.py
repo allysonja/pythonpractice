f9e654a5fb30efc48f2736ebe44dd05b20622a15
Sonja Linton <sonja.linton@maxxpotential.com>
2018-06-14 13:00:10 -0400

    user input on number of times to repeat message and repeated message

--- pycode.py
+++ pycode.py
@@ -2,7 +2,9 @@ import datetime
 
 name = input("What is your name?: ")
 age = int(input("What is your age?: "))
+repeat = int(input("How many times should I repeat your message?: "))
 
 age100 = datetime.datetime.now().year - age + 100
 
-print("Hello " + name + "! You will turn 100 years old in the year " + str(age100) + ".")
+for i in range(0, repeat):
+	print("Hello " + name + "! You will turn 100 years old in the year " + str(age100) + ".")
