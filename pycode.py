6d6d9eb7906ef3e337352097ac04a19c530ed9fe
Sonja Linton <sonja.linton@maxxpotential.com>
2018-06-14 12:57:52 -0400

    added customized message with name and year of age 100

--- pycode.py
+++ pycode.py
@@ -4,4 +4,5 @@ name = input("What is your name?: ")
 age = int(input("What is your age?: "))
 
 age100 = datetime.datetime.now().year - age + 100
-print(age100)
+
+print("Hello " + name + "! You will turn 100 years old in the year " + str(age100) + ".")
